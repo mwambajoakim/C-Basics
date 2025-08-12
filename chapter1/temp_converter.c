1bd415da42f025138dac83b5274fd074fd1ae031
mwamba <mwambajoakim@gmail.com>
2025-08-12 21:31:03 +0300

    Convversion table for temparature

--- chapter1/temp_converter.c
+++ chapter1/temp_converter.c
@@ -15,10 +15,10 @@ int main(void)
 	step = 20;
 
 	fahr = lower;
-	while(fahr < upper)
+	while (fahr < upper)
 	{
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n",fahr, celsius);
+		printf("%d\t%d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 	return (0);
